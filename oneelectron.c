ff34d605291b23dd271a3d73eceb759c5c9f9bda
Yan Lukashevich <yanlukashevich2@gmail.com>
2023-01-17 13:14:57 +0100

    dobra wersja programu króry liczy czas który electron traci na pokonanie odległości(z najprostszym modelem udeżenia)

--- oneelectron.c
+++ oneelectron.c
@@ -18,7 +18,7 @@ struct electron create_electron(void){
     el.y=rand()%101;
     el.vx=rand()%20-10;
     el.vy=rand()%20-10;
-  //printf("x = %f  y = %f  vx = %f  vy = %f\n", a.x, a.y, a.vx, a.vy);
+    //printf("x = %f  y = %f  vx = %f  vy = %f\n", el.x, el.y, el.vx, el.vy);
     return el;
 }
 
@@ -29,7 +29,7 @@ void onesecond(struct electron *el, float boost){
     el->x=el->x + el->vx;
     el->y=el->y + el->vy;
     el->vx = el->vx + boost; 
-    printf("x = %f  y = %f  vx = %f  vy = %f\n", el->x, el->y, el->vx, el->vy);
+    //printf("x = %f  y = %f  vx = %f  vy = %f\n", el->x, el->y, el->vx, el->vy);
 }
 
 //wypełnia tablice atomów nadając centry atomów x(0;1000) y(0;100)
@@ -43,14 +43,14 @@ void create_atoms(struct atom * atom_arr){
         }
     }
     /*for(i=0;i<300;i++){
-        printf("i=%d  x=%f y=%f\n",i , (atom_arr+i)->x, (atom_arr+i)->y);
+        printf("number atomu=%d  x=%f y=%f\n",i , (atom_arr+i)->x, (atom_arr+i)->y);
     }*/
 }
 //zeruje prędkość vx jeżeli electron zdaża się z atomem i kontroluje że by nie wylatał z pola y(0;100)
 //dla sprawdzenia udeżenia electrona wykorzystuje się wzór (x-a)^2+(x-b)^2<r^2
 //wspx=(x-a)^2 wspy=(y-a)^2 
 void collision_check(struct electron * el, struct atom * atom_arr){
-    int i;
+    int i=0;
     float wspx, wspy, r2;
     r2=25;
     if(el->y > 100){
@@ -59,37 +59,41 @@ void collision_check(struct electron * el, struct atom * atom_arr){
     if(el->y < 0){
         el->y=el->y+100;
     }
-    for(i=0;i<300;i++){
-            if(((el->x - (atom_arr+i)->x)*(el->x - (atom_arr+i)->x)+(el->y - (atom_arr+i)->y)*(el->y - (atom_arr+i)->y)) < r2){
-                el->vx=0;
-            }
-        }
-    /*hciałem skrócić ten olbrzymi for u gury ale coś nie wychodi 
-    wspx=(el->x - (atom_arr+i)->x)*(el->x - (atom_arr+i)->x);
-    wspy=(el->y - (atom_arr+i)->y)*(el->y - (atom_arr+i)->y);
-    for(i=0;i<300;i++){
-        if( (wspx+wspy)<r2){
+    /*for(i=0;i<10;i++){
+        if(((el->x - (atom_arr+i)->x)*(el->x - (atom_arr+i)->x)+(el->y - (atom_arr+i)->y)*(el->y - (atom_arr+i)->y)) < r2){
+            printf("n=%d ",i);
+            printf("\n\ncolizion\n");
+            printf("wspx =%f", (el->x - (atom_arr+i)->x)*(el->x - (atom_arr+i)->x));
+            printf("wspy =%f\n", (el->y - (atom_arr+i)->y)*(el->y - (atom_arr+i)->y));
             el->vx=0;
         }
     }*/
+
+    for(i=0;i<300;i++){
+        wspx=(el->x - (atom_arr+i)->x)*(el->x - (atom_arr+i)->x);
+        wspy=(el->y - (atom_arr+i)->y)*(el->y - (atom_arr+i)->y);
+        if((wspx+wspy)<r2){
+            el->vx=0;
+        }
+    }
 }
 
 //pole ma rozmiar x(0;1000) y(0;100)
 int main(){
-    int i, n;
+    int n;
     float boost=5;
-    struct electron amain;
+    struct electron el;
     struct atom atom_arr[300];
 
-    amain=create_electron();
+    el=create_electron();
     create_atoms(atom_arr);
 
     //robi iteracji póki electron nie skonczy droge
     for(n=0;;n++){
-        onesecond(&amain, boost);
-        collision_check(&amain, atom_arr);
+        onesecond(&el, boost);
+        collision_check(&el, atom_arr);
         //kończy programm jeśli współrzędna x>1000
-        if(amain.x > 1000){
+        if(el.x > 1000){
             break;
         }
     }
